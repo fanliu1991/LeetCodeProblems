331a4512ba9a4e64efe47664a7d188302ce1b0f4
Fan Liu <liufanl@sfu.ca>
2018-05-29 16:59:24 -0700

    Update 15_3Sum.py

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -37,7 +37,7 @@ class Solution(object):
         nums.sort()
         for i in xrange(len(nums) - 2):
             if i > 0 and nums[i] == nums[i-1]:
-                continue #  The continue statement rejects all the remaining statements in the current iteration of the loop and moves the control back to the top of the loop.
+                continue # The continue statement rejects all the remaining statements in the current iteration of the loop and moves the control back to the top of the loop.
             left, right = i+1, len(nums) - 1
             while left < right:
                 three_sum = nums[i] + nums[left] + nums[right]
@@ -47,12 +47,13 @@ class Solution(object):
                     right -= 1
                 else:
                     res.append([nums[i], nums[left], nums[right]])
+                    # find the last repeated left number and the first repeated right number
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -=1
                     left += 1
                     right -= 1
-                    if nums[left] == nums[left-1]:
-                        left += 1
-                    if nums[right] == nums[right+1]:
-                        right -=1
 
         return res
 
