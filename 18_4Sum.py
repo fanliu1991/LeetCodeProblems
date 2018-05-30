cb88faf853ef79bb50228f152efc37c88b87e7ed
Fan Liu <liufanl@sfu.ca>
2018-05-30 15:20:14 -0700

    Update 18_4Sum.py

--- 18_4Sum.py
+++ 18_4Sum.py
@@ -25,8 +25,72 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+
+        # method 1: use sum2 dictionary
+        twosum = {}
+        result = []
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                sum_ij = nums[i]+nums[j]
+                if sum_ij in twosum:
+                    twosum[sum_ij].append((i, j))
+                else:
+                    twosum[sum_ij] = [(i, j)]
+
+        for sum2 in twosum:
+            diff = target - sum2
+            if diff in twosum:
+                for i, j in twosum[sum2]:
+                    for m, n in twosum[diff]:
+                        if i != m and i != n and j != m and j != n:
+                            quadruplet = sorted([nums[i], nums[j], nums[m], nums[n]])
+                            if quadruplet not in res:
+                                result.append(quadruplet)
+
+        return result
         
+        # method 2: 
+        """
+        result = []
+        length = len(nums)
+        nums.sort()
         
+        for i in range(0, len(nums)-3):
+            if i != 0 and nums[i] == nums[i-1]:
+                continue
+            
+            if nums[i]*4 > target: # since nums[i] is the min number in nums[i:]
+                break
+
+            for j in range(len(nums)-1, i+2, -1):
+                if j != len(nums)-1 and nums[j] == nums[j+1]:
+                    continue
+
+                if nums[j]*4 < target: # since nums[j] is the max number in nums[:j]
+                    break
+
+                sub2 = target - nums[i] - nums[j]
+                low, high = i+1, j-1
+                while low < high:
+                    sum2 = nums[low] + nums[high]
+                    if sum2 > sub2:
+                        high -= 1
+                    elif sum2 < sub2:
+                        low += 1
+                    else:
+                        ans = [nums[i], nums[low], nums[high], nums[j]]
+                        ans.sort()
+                        result.append(ans)
+                        while low<high and nums[low+1] == nums[low]:
+                            low+=1
+                        while low<high and nums[high-1] == nums[high]:
+                            high-=1
+                        high, low = high-1, low+1
+        
+        return result
+        """
+      
         # general method
         """
         results = []
@@ -60,29 +124,6 @@ class Solution(object):
         findNum(nums, target, 4, [], results)
         return results
         """
-      
-        twosum = {}
-        res = []
-
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                sum_ij = nums[i]+nums[j]
-                if sum_ij in twosum:
-                    twosum[sum_ij].append((i, j))
-                else:
-                    twosum[sum_ij] = [(i, j)]
-
-        for sum2 in twosum:
-            diff = target - sum2
-            if diff in twosum:
-                for i, j in twosum[sum2]:
-                    for m, n in twosum[diff]:
-                        if i != m and i != n and j != m and j != n:
-                            quadruplet = sorted([nums[i], nums[j], nums[m], nums[n]])
-                            if quadruplet not in res:
-                                res.append(quadruplet)
-
-        return res
 
 
 
